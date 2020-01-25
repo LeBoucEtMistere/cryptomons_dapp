bf882879c0ae19dc7ea103897214124e364488e3
Depasse Arthur <arthur.depasse96@gmail.com>
2020-01-25 23:19:22 +0000

    source addresses from env for heroku

--- src/util/constants.js
+++ src/util/constants.js
@@ -9,7 +9,11 @@ export const constants = {
     "5777": "Ganache Blockchain"
   },
   addresses: {
-    CryptomonContract: "0xB75BD31D13FdFd59A1B16eE2062C196ec6db2dA0",
-    MarketContract: "0xdF12D74863C7920aFacD8EA6dE9ead5Fd84db708"
+    CryptomonContract:
+      process.env.MARKET_ADDRESS ||
+      "0xB75BD31D13FdFd59A1B16eE2062C196ec6db2dA0",
+    MarketContract:
+      process.env.CRYPTOMON_ADDRESS ||
+      "0xdF12D74863C7920aFacD8EA6dE9ead5Fd84db708"
   }
 };
